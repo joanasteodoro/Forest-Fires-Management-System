fedc525228ac20216dffa4e530fed9eb40b3a78a
João Andrade <joao.ag.andrade@gmail.com>
2018-11-29 10:55:38 +0000

    Can now insert diferent types of means with same id Needs checking on remaining functionalities

--- entrega3/insertMean.php
+++ entrega3/insertMean.php
@@ -16,14 +16,18 @@
         $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+	
+        $sql3 = "SELECT * FROM meio WHERE (numMeio = :numMeio AND nomeEntidade = :nomeEntidade) ;";
         $sql1 = "INSERT INTO meio values (:numMeio, :nomeMeio, :nomeEntidade);";
         $sql2 = "INSERT INTO $table values (:numMeio, :nomeEntidade);";
         echo("<p>$table</p>");
         //echo("<p>$PS</p>");
 
 
-        $result1 = $db->prepare($sql1);
-        $result1->execute([':numMeio' => $numMeio, ':nomeEntidade' => $entidade, ':nomeMeio' => $nomeMeio]);
+        if($result3->rowCount() == 0){
+	    $result1 = $db->prepare($sql1);
+            $result1->execute([':numMeio' => $numMeio, ':nomeEntidade' => $entidade, ':nomeMeio' => $nomeMeio]);
+        }
 
         $result2 = $db->prepare($sql2);
         $result2->execute([':numMeio' => $numMeio, ':nomeEntidade' => $entidade]);
