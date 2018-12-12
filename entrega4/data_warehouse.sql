1a4b5f83298c6952bbdf3875b89fe44004694e56
joanasteodoro <joanasilvateodoro@gmail.com>
2018-12-12 12:17:44 +0000

    load meio done corgis

--- entrega4/data_warehouse.sql
+++ entrega4/data_warehouse.sql
@@ -1,22 +1,22 @@
 drop table if exists d_evento cascade;
 drop table if exists d_meio cascade;
 drop table if exists d_tempo cascade;
-drop table if exists d_cena cascade;
+drop table if exists d_facto cascade;
 
 
 create table d_evento(
-  idEvento serial not null unique,
   numTelefone varchar(9) not null,
   instanteChamada timestamp not null,
+  idEvento serial not null,
   primary key(idEvento)
 );
 
 create table d_meio(
-  idMeio serial not null unique,
   numMeio integer not null,
   nomeMeio varchar(60) not null,
   nomeEntidade varchar(60) not null,
   tipo varchar(60) not null,
+  idMeio serial not null,
   primary key(idMeio)
 );
 
@@ -24,17 +24,16 @@ create table d_tempo(
   dia integer not null,
   mes integer not null,
   ano integer not null,
-  primary key(dia, mes, ano)
+  idTempo serial not null,
+  primary key(idTempo)
 );
 
 create table d_facto(
   idEvento integer not null,
   idMeio integer not null,
-  dia integer not null,
-  mes integer not null,
-  ano integer not null,
+  idTempo integer not null,
   foreign key(idEvento) references d_evento(idEvento),
   foreign key(idMeio) references d_meio(idMeio),
-  foreign key(dia, mes, ano) references d_tempo(dia, mes, ano),
-  primary key(idEvento, idMeio)
+  foreign key(idTempo) references d_tempo(idTempo),
+  primary key(idEvento, idMeio, idTempo)
 );
--- entrega4/loadevento.sql
+++ entrega4/loadevento.sql
@@ -1,45 +1,33 @@
 CREATE OR REPLACE FUNCTION load_d_evento()
 RETURNS VOID AS
 $$
-DECLARE count INTEGER DEFAULT 1;
-DECLARE evento eventoEmergencia%ROWTYPE;
-DECLARE cursor_event CURSOR FOR
-SELECT * FROM eventoEmergencia;
 BEGIN
-  OPEN cursor_event;
-  LOOP
-    FETCH cursor_event INTO evento;
-    EXIT WHEN NOT FOUND;
-    INSERT INTO d_evento VALUES(count, evento.numTelefone, evento.instanteChamada);
-    count := count + 1;
-  END LOOP;
-  CLOSE cursor_event;
+  INSERT INTO d_evento
+  SELECT numTelefone, instanteChamada
+  FROM eventoEmergencia;
 END;
 $$ LANGUAGE plpgsql;
 
-DO $$ BEGIN
-  PERFORM load_d_evento();
-END $$;
 
 CREATE OR REPLACE FUNCTION load_d_meio()
 RETURNS VOID AS
 $$
-DECLARE count INTEGER DEFAULT 1;
-DECLARE meio meio%ROWTYPE;
-DECLARE cursor_event CURSOR FOR
-SELECT * FROM eventoEmergencia;
 BEGIN
-  OPEN cursor_event;
-  LOOP
-    FETCH cursor_event INTO evento;
-    EXIT WHEN NOT FOUND;
-    INSERT INTO d_evento VALUES(count, evento.numTelefone, evento.instanteChamada);
-    count := count + 1;
-  END LOOP;
-  CLOSE cursor_event;
+  INSERT INTO d_meio
+  SELECT numMeio, nomeMeio, nomeEntidade, 'Apoio'
+  FROM meioApoio NATURAL JOIN meio;
+
+  INSERT INTO d_meio
+  SELECT numMeio, nomeMeio, nomeEntidade, 'Socorro'
+  FROM meioSocorro NATURAL JOIN meio;
+
+  INSERT INTO d_meio
+  SELECT numMeio, nomeMeio, nomeEntidade, 'Combate'
+  FROM meioCombate NATURAL JOIN meio;
 END;
 $$ LANGUAGE plpgsql;
 
 DO $$ BEGIN
   PERFORM load_d_evento();
+  PERFORM load_d_meio();
 END $$;
